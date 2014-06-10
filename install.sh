eeb2da1d2150d447669d575224c6426bc182e97b
Simon Beattie <mail@simonbeattie.net>
2014-06-10 10:29:07 +0100

    Updated install.sh to set file permissions

--- install.sh
+++ install.sh
@@ -3,16 +3,18 @@
 echo "Installing..."
 
 if [ -f reports.sqlite ]; then
-    echo "System already installed!"
-    exit 1
-
-    chmod 777
+    echo "System already installed, setting permissions!"
+    chmod 777 reports.sqlite
+    chmod 777 Web/uploads
 fi
 
 echo "Copying Database"
 cp reports.sqlite.template reports.sqlite
 echo "Copying config"
 cp Web/config.php.template Web/config.php
+echo "Setting permissions"
+    chmod 777 reports.sqlite
+    chmod 777 Web/uploads
 
 echo  "Complete"
 exit 0
