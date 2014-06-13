7e983b19054c8f592cf265fbc76e3b9b63e159c7
Simon Beattie <mail@simonbeattie.net>
2014-06-13 08:38:13 +0100

    Added config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * slim -- config.php
+ * User: Simon Beattie
+ * Date: 10/06/2014
+ * Time: 15:55
+ */
+
+date_default_timezone_set('Europe/London');
+
+return array(
+    'db' => array(
+        'hostname'  =>  '127.0.0.1',
+        'database'  =>  'reports',
+        'username'  =>  'root',
+        'password'  =>  'pa55word'
+    ),
+    'sqlite3' => 'Database/reports.sqlite',
+    'severity' => '4'
+);
