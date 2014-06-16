e76aedf4199ebd3b5823ce615c605a62014dd744
Simon Beattie <mail@simonbeattie.net>
2014-06-16 14:57:52 +0100

    Created bootstrap include file

---
+++ Library/bootstrap.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * nessus-report-parser -- bootstrap.php
+ * User: Simon Beattie
+ * Date: 16/06/2014
+ * Time: 14:52
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+
+spl_autoload_register(function($className)
+{
+    $fileName = __DIR__ . '/' . str_replace('\\', '/', $className) . '.php';
+
+    if(!file_exists($fileName))
+    {
+        return false;
+    }
+
+    require($fileName);
+});
+
+
+if (file_exists(__DIR__ . '/../config.php'))
+{
+    $config = require(__DIR__ . '/../config.php');
+}
+else
+{
+    die('Config.php does not exist - Please run install.sh');
+}
+
+
+try {
+    $pdo = new \PDO(
+        'mysql:host=' . $config['db']['hostname'] . ';dbname=' . $config['db']['database'],
+        $config['db']['username'],
+        $config['db']['password']
+    );
+} catch (\PDOException $pdoError)
+{
+    print $pdoError->getMessage() . PHP_EOL;
+}
--- index.php
+++ index.php
@@ -6,42 +6,7 @@
  * Time: 15:34
  */
 
-require 'vendor/autoload.php';
-
-spl_autoload_register(function($className)
-{
-    $fileName = __DIR__ . '/' . str_replace('\\', '/', $className) . '.php';
-
-    if(!file_exists($fileName))
-    {
-        return false;
-    }
-
-    require($fileName);
-});
-
-
-if (file_exists(__DIR__ . '/config.php'))
-{
-    $config = require(__DIR__ . '/config.php');
-}
-else
-{
-    die('Config.php does not exist - Please run install.sh');
-}
-
-
-try {
-    $pdo = new \PDO(
-        'mysql:host=' . $config['db']['hostname'] . ';dbname=' . $config['db']['database'],
-        $config['db']['username'],
-        $config['db']['password']
-    );
-} catch (\PDOException $pdoError)
-{
-    print $pdoError->getMessage() . PHP_EOL;
-}
-
+include_once('Library/bootstrap.php');
 
 $app = new \Slim\Slim(array(
     'templates.path' => './views'
